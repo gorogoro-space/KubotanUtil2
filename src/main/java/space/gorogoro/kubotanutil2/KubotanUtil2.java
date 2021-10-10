d3f59219f588b5739e996fc94d50caed516173d6
kubotan <admin@gorogoro.space>
2021-10-10 15:49:18 +0900

    bugfix

--- src/main/java/space/gorogoro/kubotanutil2/KubotanUtil2.java
+++ src/main/java/space/gorogoro/kubotanutil2/KubotanUtil2.java
@@ -26,9 +26,9 @@ public class KubotanUtil2 extends JavaPlugin implements Listener{
     try{
       BukkitUtil.logInfo("The Plugin Has Been Enabled!");
       getServer().getPluginManager().registerEvents(this, this);
-      
+
       // 設定ファイルが無ければ作成
-      File configFile = new File(this.getDataFolder() + "/config.yml");
+      File configFile = new File(getDataFolder(), "config.yml");
       if(!configFile.exists()){
         this.saveDefaultConfig();
       }
@@ -36,137 +36,69 @@ public class KubotanUtil2 extends JavaPlugin implements Listener{
       // JDBCドライバーの指定
       Class.forName("org.sqlite.JDBC");
       // データベースに接続する なければ作成される
-      con = DriverManager.getConnection("jdbc:sqlite:" + this.getDataFolder() + "/status.db");
+      con = DriverManager.getConnection("jdbc:sqlite:" + getDataFolder() + File.separator + "status.db");
       con.setAutoCommit(false);      // auto commit無効
 
-      // ResultSet及び、Statementオブジェクト作成
-      ResultSet rs;
+      // Statementオブジェクト作成
       Statement stmt = con.createStatement();
       stmt.setQueryTimeout(30);    // タイムアウト設定
 
-      // テーブルの実在チェック
-      Boolean existsUserTable = false;
-      rs = stmt.executeQuery("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='users'");
-      while (rs.next()) {
-        if(rs.getString(1).equals("1")){
-          existsUserTable = true;
-        }
-      }
-      Boolean existsHistoryTable = false;
-      rs = stmt.executeQuery("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='last_executed_history'");
-      while (rs.next()) {
-        if(rs.getString(1).equals("1")){
-          existsHistoryTable = true;
-        }
-      }
+      // テーブル作成
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users ("
+        + " id INTEGER PRIMARY KEY AUTOINCREMENT"
+        + ",uuid STRING NOT NULL"
+        + ",playername STRING NOT NULL"
+        + ",reputation REAL NOT NULL DEFAULT 1.000"
+        + ",reason STRING NOT NULL DEFAULT 'Empty for old data.'"
+        + ",is_jailed INTEGER NOT NULL DEFAULT 0"
+        + ",is_baned INTEGER NOT NULL DEFAULT 0"
+        + ",last_executed_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(last_executed_at LIKE '____-__-__ __:__:__')"
+        + ",created_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(created_at LIKE '____-__-__ __:__:__')"
+        + ",updated_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(updated_at LIKE '____-__-__ __:__:__')"
+        + ");"
+      );
+      stmt.executeUpdate("CREATE UNIQUE INDEX IF NOT EXISTS uuid_uindex ON users (uuid);");
+      stmt.executeUpdate("CREATE INDEX IF NOT EXISTS playername_index ON users (playername);");
 
-      // 理由の項目の実在チェック
-      Boolean existsHistoryReasonField = false;
-      rs = stmt.executeQuery("SELECT count(*) from sqlite_master  where name = 'last_executed_history' and sql like '%reason%'");
-      while (rs.next()) {
-        if(rs.getString(1).equals("1")){
-          existsHistoryReasonField = true;
-        }
-      }
-
-      // 理由の項目の実在チェック
-      Boolean existsIsJailedField = false;
-      rs = stmt.executeQuery("SELECT count(*) from sqlite_master  where name = 'users' and sql like '%is_jailed%'");
-      while (rs.next()) {
-        if(rs.getString(1).equals("1")){
-          existsIsJailedField = true;
-        }
-      }
-      Boolean existsIsBanedField = false;
-      rs = stmt.executeQuery("SELECT count(*) from sqlite_master  where name = 'users' and sql like '%is_baned%'");
-      while (rs.next()) {
-        if(rs.getString(1).equals("1")){
-          existsIsBanedField = true;
-        }
-      }
-      
-      // テーブルが無かった場合
-      if(!existsUserTable){
-        // テーブル作成
-        stmt.executeUpdate("CREATE TABLE users ("
-          + " id INTEGER PRIMARY KEY AUTOINCREMENT"
-          + ",uuid STRING NOT NULL"
-          + ",playername STRING NOT NULL"
-          + ",reputation REAL NOT NULL DEFAULT 1.000"
-          + ",reason STRING NOT NULL DEFAULT 'Empty for old data.'"
-          + ",is_jailed INTEGER NOT NULL DEFAULT 0"
-          + ",is_baned INTEGER NOT NULL DEFAULT 0"
-          + ",last_executed_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(last_executed_at LIKE '____-__-__ __:__:__')"
-          + ",created_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(created_at LIKE '____-__-__ __:__:__')"
-          + ",updated_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(updated_at LIKE '____-__-__ __:__:__')"
-          + ");"
-        );
-        //インデックス作成
-        stmt.executeUpdate("CREATE INDEX uuid ON users (uuid);");
-        stmt.executeUpdate("CREATE INDEX playername ON users (playername);");
-      }
-      // テーブルが無かった場合
-      if(!existsHistoryTable){
-        // テーブル作成
-        stmt.executeUpdate("CREATE TABLE last_executed_history ("
-          + " id INTEGER PRIMARY KEY AUTOINCREMENT"
-          + ",sender_uuid STRING NOT NULL"
-          + ",sender_playername STRING NOT NULL"
-          + ",target_uuid STRING NOT NULL"
-          + ",target_playername STRING NOT NULL"
-          + ",last_executed_type INTEGER NOT NULL"
-          + ",reason STRING NOT NULL DEFAULT 'Empty for old data'"
-          + ",created_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(created_at LIKE '____-__-__ __:__:__')"
-          + ",updated_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(updated_at LIKE '____-__-__ __:__:__')"
-          + ",unique(sender_uuid, target_uuid, last_executed_type)"
-          + ");"
-        );
-        //インデックス作成
-        stmt.executeUpdate("CREATE INDEX last_uuid_type ON last_executed_history (sernder_uuid, target_uuid, last_executed_type);");
-        stmt.executeUpdate("CREATE INDEX sender_playername ON last_executed_history (sender_playername);");
-        stmt.executeUpdate("CREATE INDEX target_playername ON last_executed_history (target_playername);");
-      }
-
-      // 理由の項目が無かった場合
-      if(!existsHistoryReasonField){
-        // 理由の追加
-        stmt.executeUpdate("ALTER TABLE last_executed_history ADD COLUMN reason STRING NOT NULL DEFAULT 'Empty for old data';");
-      }
-
-      // 投獄済みの項目が無かった場合
-      if(!existsIsJailedField){
-        // 投獄フラグの追加
-        stmt.executeUpdate("ALTER TABLE users ADD COLUMN is_jailed INTEGER NOT NULL DEFAULT 0;");
-      }
-
-      // BAN済みの項目が無かった場合
-      if(!existsIsBanedField){
-        // BANフラグの追加
-        stmt.executeUpdate("ALTER TABLE users ADD COLUMN is_baned INTEGER NOT NULL DEFAULT 0;");
-      }
-      
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS last_executed_history ("
+        + " id INTEGER PRIMARY KEY AUTOINCREMENT"
+        + ",sender_uuid STRING NOT NULL"
+        + ",sender_playername STRING NOT NULL"
+        + ",target_uuid STRING NOT NULL"
+        + ",target_playername STRING NOT NULL"
+        + ",last_executed_type INTEGER NOT NULL"
+        + ",reason STRING NOT NULL DEFAULT 'Empty for old data'"
+        + ",created_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(created_at LIKE '____-__-__ __:__:__')"
+        + ",updated_at DATETIME NOT NULL DEFAULT (datetime('now','localtime')) CHECK(updated_at LIKE '____-__-__ __:__:__')"
+        + ",unique(sender_uuid, target_uuid, last_executed_type)"
+        + ");"
+      );
+      stmt.executeUpdate("CREATE INDEX IF NOT EXISTS last_uuid_type_index ON last_executed_history (sernder_uuid, target_uuid, last_executed_type);");
+      stmt.executeUpdate("CREATE INDEX IF NOT EXISTS sender_playername_index ON last_executed_history (sender_playername);");
+      stmt.executeUpdate("CREATE INDEX IF NOT EXISTS target_playername_index ON last_executed_history (target_playername);");
       stmt.close();
+
     } catch (Exception e){
       BukkitUtil.logStackMessage(e);
     }
-
   }
 
   @EventHandler
   public void onPlayerJoinEvent(PlayerJoinEvent event){
     try {
       // 既存ユーザーか確認
-      String updateUser = "";
+      String dbPlayerName; 
+      String queryUpdateUser = "";
       Boolean existsUser = false;
       PreparedStatement prepStmt = con.prepareStatement("SELECT playername FROM users WHERE uuid=?");
       prepStmt.setString(1, event.getPlayer().getUniqueId().toString());
       ResultSet rs = prepStmt.executeQuery();
-      while(rs.next()){
-        String preName = rs.getString(1);
-        if(!preName.equals(event.getPlayer().getName())) {
-          updateUser = "UPDATE users SET playername=? WHERE uuid=?";
+      if (rs.next()){
+        existsUser = true;
+        dbPlayerName = rs.getString(1);
+        if(!dbPlayerName.equals(event.getPlayer().getName())) {
+          queryUpdateUser = "UPDATE users SET playername=? WHERE uuid=?";
         }
-        break;
       }
       rs.close();
       prepStmt.close();
@@ -190,8 +122,8 @@ public class KubotanUtil2 extends JavaPlugin implements Listener{
         prepStmt.close();
       }
 
-      if(!updateUser.equals("")) {
-        prepStmt = con.prepareStatement(updateUser);
+      if(!queryUpdateUser.isEmpty()) {
+        prepStmt = con.prepareStatement(queryUpdateUser);
         prepStmt.setString(1, event.getPlayer().getName());
         prepStmt.setString(2, event.getPlayer().getUniqueId().toString());
         prepStmt.addBatch();
